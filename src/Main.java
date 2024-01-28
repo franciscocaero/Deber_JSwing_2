975bcf069864bc6fba2e34107e13f1e65bd71cbe
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-01-27 19:22:20 -0500

    Update Main.java
    Cambios necesarios para mejorar el proyecto

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    static JFrame frame=new JFrame("Mi primera pantalla");
+    static JFrame frame=new JFrame("Iniciar sesión");
     public static void main(String[] args) {
 
         frame.setContentPane(new Login().log);
